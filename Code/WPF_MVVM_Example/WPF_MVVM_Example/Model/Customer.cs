52beb66577afdcfc4bdb8c0300a23af7d5594380
stuckiandreas <stuckand@students.zhaw.ch>
2017-04-15 19:44:26 +0200

    Modifikation Disposition

---
+++ Code/WPF_MVVM_Example/WPF_MVVM_Example/Model/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVVM_Example.Model
+{
+    public class Customer
+    {
+        private String _CustomerName;
+
+        public String CustomerName;
+
+        private double _Amount;
+
+        public double Amount;
+
+        private String _Married;
+
+        public String Married;
+
+        private double _Tax;
+        public double Tax
+        {
+            get { return _Tax; }
+        }
+
+        public void CalculateTax()
+        {
+            if (_Amount > 2000)
+            {
+                _Tax = 20;
+            }
+            else if (_Amount > 1000)
+            {
+                _Tax = 10;
+            }
+            else
+            {
+                _Tax = 5;
+            }
+        }
+    }
+}
---
+++ Code/WPF_MVVM_Example/WPF_MVVM_Example/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Media;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm;
+using WPF_MVVM_Example.Model;
+
+namespace WPF_MVVM_Example.ViewModels
+{
+    [POCOViewModel]
+    public class CustomerViewModel
+    {
+        private Customer obj = new Customer();
+
+        public string TxtCustomerName
+        {
+            get { return obj.CustomerName; }
+            set { obj.CustomerName = value; }
+        }
+
+        public string TxtAmount
+        {
+            get { return Convert.ToString(obj.Amount); }
+            set { obj.Amount = Convert.ToDouble(value); }
+        }
+
+
+        public string LblAmountColor
+        {
+            get
+            {
+                if (obj.Amount > 2000)
+                {
+                    return "Blue";
+                }
+                else if (obj.Amount > 1500)
+                {
+                    return "Red";
+                }
+                return "Yellow";
+            }
+        }
+
+        public bool IsMarried
+        {
+            get
+            {
+                if (obj.Married == "Married")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+
+        private void DisplayUi(CustomerViewModel o)
+        {
+            TxtCustomerName = o.TxtCustomerName;
+            /*lblAmount.Content = o.TxtAmount;
+            BrushConverter brushconv = new BrushConverter();
+            lblBuyingHabits.Background = brushconv.ConvertFromString(o.LblAmountColor) as SolidColorBrush;
+            chkMarried.IsChecked = o.IsMarried;*/
+        }
+    }
+}
