8d47cdfb011c8da6444f772e26c88f74ff3a1f1c
stuckiandreas <stuckand@students.zhaw.ch>
2017-08-01 14:22:46 +0200

    Neues lauffähiges Beispiel

--- Code/MasterDetailViewExample/MasterDetailViewExample/ViewModel/WorkspaceViewModel.cs
+++ Code/MasterDetailViewExample/MasterDetailViewExample/ViewModel/WorkspaceViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.TextFormatting;
 using MasterDetailViewExample.Model;
 
 namespace MasterDetailViewExample.ViewModel
@@ -12,9 +13,19 @@ namespace MasterDetailViewExample.ViewModel
     {
         private readonly OrderListDatabaseAccessManager orderListDatabaseAccessManager;
 
-        public List<Customer> Customers { get; private set; }
+        private List<Customer> Customers;
 
         public List<OrderList> OrderList { get; private set; }
+
+        public ObservableCollection<CustomerType> CustomerList { get; private set; }
+
+        private string flinty = "flinty";
+
+        public string Flinty
+        {
+            get { return flinty; }
+            set { flinty = value;  }
+        }
 
         public WorkspaceViewModel(OrderListDatabaseAccessManager orderListDatabaseAccessManager)
         {
@@ -22,6 +33,12 @@ namespace MasterDetailViewExample.ViewModel
 
             Customers = orderListDatabaseAccessManager.GetCustomerList();
             OrderList = orderListDatabaseAccessManager.GetOrderList();
+
+            for (int i = 0; i < Customers.Count; i++)
+            {
+                if (CustomerList == null)   CustomerList = new ObservableCollection<CustomerType>();
+                CustomerList.Add(new CustomerType(Customers[i].Surname, Customers[i].Lastname));
+            }
         }
     }
 }
