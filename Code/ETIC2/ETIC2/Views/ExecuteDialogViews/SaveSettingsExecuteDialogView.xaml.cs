d4b0f35308de4919730e05b9136e7eaaed776765
stuckiandreas <stuckand@students.zhaw.ch>
2017-08-09 15:36:25 +0200

    Lauffähige Level 1 Master Detail Version

--- Code/ETIC2/ETIC2/Views/ExecuteDialogViews/SaveSettingsExecuteDialogView.xaml.cs
+++ Code/ETIC2/ETIC2/Views/ExecuteDialogViews/SaveSettingsExecuteDialogView.xaml.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿//-----------------------------------------------------------------------
+// <copyright file="SaveSettingsExecuteDialogView.xaml.cs" company="VAT Vakuumventile AG">
+//     Copyright (c) 2017 . All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace ETIC2.Views.ExecuteDialogViews
 {
+    using System.Windows.Controls;
+
     /// <summary>
-    /// Interaction logic for SaveSettingsExecuteDialogView.xaml
+    /// View with two text input controls and a save button. Is used to save the database settings.
     /// </summary>
     public partial class SaveSettingsExecuteDialogView : UserControl
     {
         public SaveSettingsExecuteDialogView()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
--- Code/ETIC2/ETIC2/Views/InputViews/TextInputView.xaml.cs
+++ Code/ETIC2/ETIC2/Views/InputViews/TextInputView.xaml.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿//-----------------------------------------------------------------------
+// <copyright file="TextInputView.xaml.cs" company="VAT Vakuumventile AG">
+//     Copyright (c) 2017 . All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace ETIC2.Views.InputViews
 {
+    using System.Windows.Controls;
+
     /// <summary>
-    /// Interaction logic for TextInputView.xaml
+    /// Control for a single text input.
     /// </summary>
     public partial class TextInputView : UserControl
     {
         public TextInputView()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
